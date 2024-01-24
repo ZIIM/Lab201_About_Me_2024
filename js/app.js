c320c69a0800c61ddaee4a18fbab407fc140f200
John Thomas Gause III <johntgause12@gmail.com>
2024-01-23 22:34:55 -0800

    css done, html done, working on questions in js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('Js is loaded');
+'use strict';
+
+let welcome = 'Hello what is your name?';
+// let question1 = 'Do you like Dogs';
+// let question2 = 'Do you like Bacon';
+// let question3 = 'Do you like Movies';
+// let question4 = 'Do you like Money';
+// let question5 = 'Do you like working out';
+
+let welcomemsg = prompt(welcome);
+alert('Why hello there ' + welcomemsg);
+// let answer1 = prompt(question1);
+// let answer2 = prompt(question2);
+// let answer3 = prompt(question3);
+// let answer4 = prompt(question4);
+// let answer5 = prompt(question5);
+// if (question1 === true){
+//   console.log('Nice answer.');
+// }else{
+//   console.log('Why dont you like doggos :(');
+// }
+// console.log('Js is loaded');
