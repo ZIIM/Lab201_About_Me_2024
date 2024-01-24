12d56b8b8d8482f5c1df41a9076898338c627a26
John Thomas Gause III <johntgause12@gmail.com>
2024-01-24 08:29:11 -0800

    questions

--- js/app.js
+++ js/app.js
@@ -1,14 +1,49 @@
 'use strict';
 
-let welcome = 'Hello what is your name?';
-// let question1 = 'Do you like Dogs';
-// let question2 = 'Do you like Bacon';
-// let question3 = 'Do you like Movies';
+let userScore = 0;
+let ready = prompt('Would you like to play a guessing game?');
+
+if (ready === 'y' || ready === 'yes'){
+  alert('Very good, please respond to the questions with either \'y\' or \'n\'.');
+} else {
+  alert('UNSATISFACTORY!');
+}
+
+const questions = [
+  'Is the sky blue?',
+  'Do birds fly?',
+  'Is water dry?',
+  'Can cats walk?',
+  'Can dogs do complex algebra?',
+];
+
+const correctAnswers = ['yes','yes','no','yes','no'];
+
+function askQuestions(index){
+  let userAnswer = prompt(questions[index]);
+  userAnswer = userAnswer.toLowerCase();
+
+  if (userAnswer === correctAnswers[index]){
+    alert('Correct!');
+    userScore++;
+  } else {
+    alert('Incorrect!');
+  }
+}
+
+for (let i = 0; i < questions.length; i++){
+  askQuestions(i);
+}
+alert(`Thank you for playing! Your score is: ${userScore} out of ${questions.length}`);
+// let welcome = 'Hello what is your name?';
+// let question1 = 'Are dogs cool?';
+// let question2 = 'Is bacon turkey?';
+// let question3 = 'Is dune the best scifi movie ever?';
 // let question4 = 'Do you like Money';
 // let question5 = 'Do you like working out';
 
-let welcomemsg = prompt(welcome);
-alert('Why hello there ' + welcomemsg);
+// let welcomemsg = prompt(welcome);
+// alert('Why hello there ' + welcomemsg);
 // let answer1 = prompt(question1);
 // let answer2 = prompt(question2);
 // let answer3 = prompt(question3);
